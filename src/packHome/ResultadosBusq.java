e138745b48820eabaeeae795d3fca3da8932e43e
ataches <jussanchezm@correo.udistrital.edu.co>
2020-04-06 16:41:05 -0500

    Revert "Revert "Commit inicial""
    This reverts commit 7472d8b372826b0831768b7d148462ba13bc54ab.

---
+++ src/packHome/ResultadosBusq.java
@@ -0,0 +1,21 @@
+package packHome;
+
+public class ResultadosBusq {
+
+	String ciudad, tipo, clase, estado;
+
+	public ResultadosBusq(String ciudad, String tipo, String clase, String estado) {
+		this.ciudad = ciudad;
+		this.tipo = tipo;
+		this.clase = clase;
+		this.estado = estado;
+	}
+	
+	String resul(){
+		String accion =  "error";
+		for(int i=0;i<=100;i++){
+			
+		}
+		return accion;
+	}
+}
---
+++ src/packHome/borrador.java
@@ -0,0 +1,5 @@
+package packHome;
+
+public class borrador {
+
+}
